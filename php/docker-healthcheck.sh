e72fd031d0bd43488690829ed950284b0ab080f1
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2022-02-11 07:41:51 +0100

    update healthcheck

--- php/docker-healthcheck.sh
+++ php/docker-healthcheck.sh
@@ -5,7 +5,7 @@ export SCRIPT_NAME=/ping
 export SCRIPT_FILENAME=/ping
 export REQUEST_METHOD=GET
 
-#bin/console doctrine:migrations:up-to-date
+bin/console doctrine:migrations:up-to-date
 
 if cgi-fcgi -bind -connect 127.0.0.1:9000; then
   exit 0
