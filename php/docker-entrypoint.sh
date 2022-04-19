8e521c284149403f1c38a5cae2cec4af691d107e
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2022-04-19 17:21:48 +0200

    buildx

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -8,7 +8,6 @@ fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
   mkdir -p var/cache var/log public/var
-  chown www-data: var/cache var/log public/var
   bin/console pimcore:deployment:classes-rebuild --no-interaction || true
   composer dump-autoload #required to load pimcore classes after they have been installed
 fi
